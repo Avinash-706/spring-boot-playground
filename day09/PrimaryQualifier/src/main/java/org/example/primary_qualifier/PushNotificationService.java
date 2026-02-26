de912bbcda954c12866b0c195e09a591ad1f2445
Avinash Dhanuka <avunashdhanuka@gmail.com>
2026-02-26 22:22:35 +0530

    Implemented NotificationService implementations: EmailNotificationService with @Primary, SmsNotificationService, PushNotificationService demonstrating multiple bean candidates

---
+++ day09/PrimaryQualifier/src/main/java/org/example/primary_qualifier/PushNotificationService.java
@@ -0,0 +1,14 @@
+package org.example.primary_qualifier;
+
+import org.springframework.stereotype.Component;
+
+// @Component tells Spring to create this bean
+// No @Primary, so this is NOT the default choice
+@Component
+public class PushNotificationService implements NotificationService {
+    
+    @Override
+    public void sendMsg(String message) {
+        System.out.println("Push Notification: " + message);
+    }
+}
