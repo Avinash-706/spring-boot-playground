87c119d446b1de501c2900822b6c5279c41811dd
Avinash Dhanuka <avunashdhanuka@gmail.com>
2026-03-01 14:32:35 +0530

    Configured Spring with @Configuration and @ComponentScan for Bean Lifecycle demonstration

---
+++ day10/BeanLifeCycle/src/main/java/org/example/lifecycle/lifeCycleConfig.java
@@ -0,0 +1,11 @@
+package org.example.lifecycle;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+@Configuration
+@ComponentScan(basePackages = "org.example.lifecycle")
+public class lifeCycleConfig {
+
+}
