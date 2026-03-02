fda23387a9ba0cc47d82335eb6a69b4ff1aebe11
Avinash Dhanuka <avunashdhanuka@gmail.com>
2026-03-02 18:45:31 +0530

    Implemented SpringMvcApplication with @SpringBootApplication entry point, ServletInitializer for WAR deployment configuration

---
+++ day11/SpringMvc/src/main/java/com/example/SpringMvc/SpringMvcApplication.java
@@ -0,0 +1,14 @@
+package com.example.SpringMvc;
+
+import com.example.SpringMvc.Controller.StudentController;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcApplication.class, args);
+	}
+
+}
